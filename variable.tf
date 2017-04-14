a822bc10f06f082e13f2c5aedd046c31eff92e97
Jubilee Makanjuola <ojubilee@gmail.com>
2017-04-14 22:20:09 +0000

    Added AMIS lookup

--- variable.tf
+++ variable.tf
@@ -2,3 +2,12 @@ variable "aws_access_key" {}
 variable "aws_secret_key" {}
 variable "aws_region" {}
 variable "aws_default" {}
+
+variable "AMIS" {
+  type = "map"
+  default = {
+    eu-west-2 = "ami-036b7f67" 
+    us-west-2 = "ami-fa27b39a"
+    ap-northeast-1 = "ami-53624634"
+ }
+}
