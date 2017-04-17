f042a5c044e338c68540bca308628d8b06dbd0ba
Jubilee Makanjuola <ojubilee@gmail.com>
2017-04-17 00:23:30 +0000

    Added vpc, security group and instance files

---
+++ instance-vpc-sg/instance.tf
@@ -0,0 +1,14 @@
+resource "aws_instance" "test" {  
+  ami = "${lookup(var.amis, var.aws_region)}"
+  instance_type = "t2.micro"
+  
+tags {
+  Name = "test"
+ } 
+
+subnet_id = "${aws_subnet.demo-heartflow-pub-1.id}"
+key_name = "${aws_key_pair.jubkeypair.key_name}"
+vpc_security_group_ids = ["${aws_security_group.demo-heartflow-pub-sg.id}"]
+
+}
+
