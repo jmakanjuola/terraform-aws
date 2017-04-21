7cbe6d77b2ee7307888a14c6d6e342321c60d979
Jubilee Makanjuola <ojubilee@gmail.com>
2017-04-21 09:20:06 +0000

    Modified variable file

--- networking/vars.tf
+++ networking/vars.tf
@@ -1,12 +1,8 @@
 variable "aws_region" {}
 variable "aws_access_key_id" {}
 variable "aws_secret_key_id" {}
-variable "demo-heartflow_instance_type" {}
+variable "demo-abra_instance_type" {}
 variable "path_to_pub_key" {}
-#variable "AMIS"{}
 
-# variable "aws_vpc.demo-heartflow-ssh" {}
-# variable "" {}
-# variable "jubkeypair" {}
 
 
