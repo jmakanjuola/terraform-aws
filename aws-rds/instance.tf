ec70fe4c8c0dbe3cb79f3f7aca401d3f22616e67
Jubilee Makanjuola <ojubilee@gmail.com>
2017-04-21 08:11:41 +0000

    Added rds files

---
+++ aws-rds/instance.tf
@@ -0,0 +1,13 @@
+resource "aws_instance" "demo-abra-test" {
+   ami = "${lookup(var.amis, var.aws_region)}"
+   instance_type = "t2.micro"
+
+   # Public vpc subnet
+   subnet_id = "${aws_subnet.demo-abra-pub-1.id}"
+   
+  # Security Group
+   vpc_security_group_ids = ["${aws_security_group.demo-abra-test.id}"]
+  
+  # SSH pub key 
+   key_name = "${aws_key_pair.jubkeypair.key_name}"
+}
