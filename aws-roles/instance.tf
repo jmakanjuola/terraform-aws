890bcbc60c19cf73bd058ef158c42483e805d952
Jubilee Makanjuola <ojubilee@gmail.com>
2017-05-05 01:12:22 -0700

    Updated file with tag

---
+++ aws-roles/instance.tf
@@ -0,0 +1,19 @@
+resource "aws_instance" "demo-abra-test" {
+    ami = "${lookup(var.amis, var.aws_region)}"
+    instance_type = "t2.micro"
+tags {
+  Name = "test"
+}
+
+# Public VPC Subnet
+subnet_id = "${aws_subnet.demo-abra-pub-1.id}"
+
+# Security Group
+vpc_security_group_ids = ["${aws_security_group.demo-abra-instance.id}"]
+
+# SSH Pub Key
+key_name = "${aws_key_pair.jubkeypair.key_name}"
+
+#Role:
+iam_instance_profile = "${aws_iam_instance_profile.demo-abra-s3-bucket-role-instanceprofile.name}"
+}
