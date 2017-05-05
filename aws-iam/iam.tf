20d206c2a1b3470dc70c1d32c5d9cf60faf33253
Jubilee Makanjuola <ojubilee@gmail.com>
2017-05-04 22:45:17 -0700

    created aws & groups/users

--- aws-iam/iam.tf
+++ aws-iam/iam.tf
@@ -1,29 +1,29 @@
 # Admin Group
-resource "aws_iam_group" "demo-abra-admin" {
-  name = "demo-abra-admin"
+resource "aws_iam_group" "demo-abobe-stock-sre-team" {
+  name = "demo-abobe-stock-sre-team"
 }
 
-resource "aws_iam_policy_attachment" "demo-abra-attach-admin" {
-  name = "demo-abra-attach-admin"
-  groups = ["${aws_iam_group.demo-abra-admin.name}"]
-  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+resource "aws_iam_policy_attachment" "demo-abobe-stock-attach-admin" {
+  name = "demo-abobe-stock-attach-admin"
+  groups = ["${aws_iam_group.demo-abobe-stock-sre-team.name}"]
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
 }
 
 # Users group
 
-resource "aws_iam_user" "operations" {
-  name = "operations"
+resource "aws_iam_user" "nelson" {
+  name = "nelson"
 }
 
-resource "aws_iam_user" "engineering" {
-  name = "engineering"
+resource "aws_iam_user" "kam" {
+  name = "kam"
 }
 
-resource "aws_iam_group_membership" "admin-users" {
-  name = "admin-users"
+resource "aws_iam_group_membership" "sre-team" {
+  name = "sre-team"
   users = [
-      "${aws_iam_user.operations.name}",
-      "${aws_iam_user.engineering.name}",
+      "${aws_iam_user.nelson.name}",
+      "${aws_iam_user.kam.name}",
  ]
-group = "${aws_iam_group.demo-abra-admin.name}"
+group = "${aws_iam_group.demo-abobe-stock-sre-team.name}"
 }
