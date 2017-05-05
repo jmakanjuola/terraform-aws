607fd9dded111167c932708c5808bf8f5e6861ce
Jubilee Makanjuola <ojubilee@gmail.com>
2017-05-05 01:03:46 -0700

    Added public & private output

--- aws-roles/output.tf
+++ aws-roles/output.tf
@@ -1,3 +1,15 @@
-output "instance" {
+output "instance-public-ip" {
   value = "${aws_instance.demo-abra-test.public_ip}"
+}
+
+output "instance-private-ip" {
+  value = "${aws_instance.demo-abra-test.private_ip}"
+}
+
+output "instance-public-dns" {
+  value = "${aws_instance.demo-abra-test.public_dns}"
+}
+
+output "instance-private-dns" {
+  value = "${aws_instance.demo-abra-test.private_dns}"
 }
