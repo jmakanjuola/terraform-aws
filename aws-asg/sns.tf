23af2a162b6ff55a6fcaf7f4bb5948c7d3a7d39e
Jubilee Makanjuola <ojubilee@gmail.com>
2017-05-06 15:50:56 -0700

    Added auto-scaling AWS resource file

--- aws-asg/sns.tf
+++ aws-asg/sns.tf
@@ -11,6 +11,6 @@ resource "aws_autoscaling_notification" "demo-adobe-sre-team-notify" {
   notifications  = [
     "autoscaling:EC2_INSTANCE_LAUNCH",
     "autoscaling:EC2_INSTANCE_TERMINATE",
-    "autoscaling:EC2_INSTANCE_LAUNCH_ERROR",
+    "autoscaling:EC2_INSTANCE_LAUNCH_ERROR", 
   ]
 }
