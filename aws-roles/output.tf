8eb86c6bdaacd2468db6e1f44a6bb3d86df7f658
Jubilee Makanjuola <ojubilee@gmail.com>
2017-05-07 22:06:09 -0700

    Updated output.tf demo name

--- aws-roles/output.tf
+++ aws-roles/output.tf
@@ -1,14 +1,14 @@
 output "instance" {
-  value = "${aws_instance.demo-abra-test.public_ip}"
+  value = "${aws_instance.demo-adobe-sre-team-test.public_ip}"
 }
 output "instance-private-ip" {
-  value = "${aws_instance.demo-abra-test.private_ip}"
+  value = "${aws_instance.demo-adobe-sre-team-test.private_ip}"
 }
 
 output "instance-public-dns" {
-  value = "${aws_instance.demo-abra-test.public_dns}"
+  value = "${aws_instance.demo-adobe-sre-team-test.public_dns}"
 }
 
 output "instance-private-dns" {
-  value = "${aws_instance.demo-abra-test.private_dns}"
+  value = "${aws_instance.demo-adobe-sre-team-test.private_dns}"
 }
