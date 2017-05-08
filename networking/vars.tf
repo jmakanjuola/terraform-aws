064f9c2574d2454307dd6381ea4ef921faa6e4b4
Jubilee Makanjuola <ojubilee@gmail.com>
2017-05-07 23:06:48 -0700

    Update demo name

--- networking/vars.tf
+++ networking/vars.tf
@@ -1,8 +1,5 @@
 variable "aws_region" {}
 variable "aws_access_key_id" {}
 variable "aws_secret_key_id" {}
-variable "demo-abra_instance_type" {}
+variable "demo-adobe-sre-team_instance_type" {}
 variable "path_to_pub_key" {}
-
-
-
