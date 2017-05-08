7ab7b439133989ed4a99fa4795b0911e537047e6
Jubilee Makanjuola <ojubilee@gmail.com>
2017-05-07 21:53:06 -0700

    Updated instance.tf demo name

--- aws-roles/instance.tf
+++ aws-roles/instance.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "demo-abra-test" {
+resource "aws_instance" "demo-adobe-sre-team-test" {
     ami = "${lookup(var.amis, var.aws_region)}"
     instance_type = "t2.micro"
 
@@ -7,14 +7,14 @@ tags {
 }
 
 # Public VPC Subnet
-subnet_id = "${aws_subnet.demo-abra-pub-1.id}"
+subnet_id = "${aws_subnet.demo-adobe-sre-team-pub-1.id}"
 
 # Security Group
-vpc_security_group_ids = ["${aws_security_group.demo-abra-instance.id}"]
+vpc_security_group_ids = ["${aws_security_group.demo-adobe-sre-team-instance.id}"]
 
 # SSH Pub Key
 key_name = "${aws_key_pair.jubkeypair.key_name}"
 
 #Role:
-iam_instance_profile = "${aws_iam_instance_profile.demo-abra-s3-bucket-role-instanceprofile.name}"
+iam_instance_profile = "${aws_iam_instance_profile.demo-adobe-sre-team-s3-bucket-role-instanceprofile.name}"
 }
