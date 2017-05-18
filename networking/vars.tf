ade04a53f2109d06b31fd1fd91ca53cb82ed02c6
Jubilee Makanjuola <ojubilee@gmail.com>
2017-05-18 13:40:55 -0700

    Update instance type variable

--- networking/vars.tf
+++ networking/vars.tf
@@ -1,7 +1,7 @@
 variable "aws_region" {}
 variable "aws_access_key_id" {}
 variable "aws_secret_key_id" {}
-variable "demo-adobe-sre-team_instance_type" {
+variable "demo-medal_instance_type" {
     default = 2
 }
 variable "path_to_pub_key" {
