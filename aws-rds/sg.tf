5470a072d805c59ffaa871a489ea4480c77a49e6
Jubilee Makanjuola <ojubilee@gmail.com>
2017-05-17 23:55:55 -0700

    Update files

--- aws-rds/sg.tf
+++ aws-rds/sg.tf
@@ -1,5 +1,5 @@
-resource "aws_security_group" "demo-adobe-sre-team-test" {
-  vpc_id = "${aws_vpc.demo-adobe-sre-team.id}"
+resource "aws_security_group" "demo-medal-test" {
+  vpc_id = "${aws_vpc.demo-medal.id}"
   name = "demo-hearflow-pub-sg"
   description = "Allows ssh ingress & egress traffic"
   ingress {
@@ -15,19 +15,19 @@ resource "aws_security_group" "demo-adobe-sre-team-test" {
     cidr_blocks = ["0.0.0.0/0"]
 }
   tags {
-    Name = "demo-adobe-sre-team-test"
+    Name = "demo-medal-test"
  }
 }
 
 resource "aws_security_group""mysqldb-sg" {
-  vpc_id = "${aws_vpc.demo-adobe-sre-team.id}"
+  vpc_id = "${aws_vpc.demo-medal.id}"
   name = "mysqldb-sg"
   description = "mysqldb access"
   ingress {
     from_port = 3306
     to_port = 3306
     protocol = "tcp"
-    security_groups = ["${aws_security_group.demo-adobe-sre-team-test.id}"]
+    security_groups = ["${aws_security_group.demo-medal-test.id}"]
 
 }
  egress {
