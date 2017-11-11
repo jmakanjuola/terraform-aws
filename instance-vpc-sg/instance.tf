440fb3c4229ac238b23f4e517bed2ea3fbf1ef65
Jubilee Makanjuola <ojubilee@gmail.com>
2017-11-11 11:37:24 -0800

    Update tag names

--- instance-vpc-sg/instance.tf
+++ instance-vpc-sg/instance.tf
@@ -1,12 +1,13 @@
-resource "aws_instance" "medal-demo" {
+resource "aws_instance" "mylab-demo" {
   ami = "${lookup(var.amis, var.aws_region)}"
   instance_type = "t2.micro"
+  count = 6
 tags {
-  Name = "medal-demo"
+  Name = "mylab-demo"
  }
 
-subnet_id = "${aws_subnet.demo-medal-pub-1.id}"
+subnet_id = "${aws_subnet.demo-mylab-pub-1.id}"
 key_name = "${aws_key_pair.jubkeypair.key_name}"
-vpc_security_group_ids = ["${aws_security_group.demo-medal-pub-sg.id}"]
+vpc_security_group_ids = ["${aws_security_group.demo-mylab-pub-sg.id}"]
 
 }
